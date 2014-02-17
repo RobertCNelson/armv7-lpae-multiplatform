2d2a0a2a6c31f815550a1fc290b7accd83fc061c
Robert Nelson <robertcnelson@gmail.com>
2014-02-16 20:26:08 -0600

    3.14-rc3-armv7-lpae-x2 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,8 +28,8 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.14
-KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=armv7-lpae-x1.2
+KERNEL_TAG=${KERNEL_REL}-rc3
+BUILD=armv7-lpae-x2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
