ddb786ffc24a638ed3b9076f3274a34bcdc09efe
Robert Nelson <robertcnelson@gmail.com>
2014-05-19 12:53:55 -0500

    3.15-rc5-armv7-lpae-x3 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.15
 KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=armv7-lpae-x2.2
+BUILD=armv7-lpae-x3
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
