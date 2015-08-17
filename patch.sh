87150dce26ec62f2f6cb953b428fa0e1790cefe5
Robert Nelson <robertcnelson@gmail.com>
2015-08-17 09:49:46 -0500

    4.1.6-armv7-lpae-x1 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -99,8 +99,6 @@ fixes () {
 		start_cleanup
 	fi
 
-	${git} "${DIR}/patches/fixes/0001-trusty-gcc-4.8-4.8.2-19ubuntu1-has-fix.patch"
-
 	if [ "x${regenerate}" = "xenable" ] ; then
 		number=1
 		cleanup
@@ -124,7 +122,7 @@ quieter () {
 
 ###
 backports
-fixes
+#fixes
 quieter
 
 packaging () {
