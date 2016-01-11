983d1abce8d484fc8f9ce997f33d8a37122b7cd1
Robert Nelson <robertcnelson@gmail.com>
2016-01-10 20:07:21 -0600

    4.4-armv7-lpae-x1 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -20,8 +20,8 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}-rc8
-BUILD=armv7-lpae-x0.1
+KERNEL_TAG=${KERNEL_REL}
+BUILD=armv7-lpae-x1
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
