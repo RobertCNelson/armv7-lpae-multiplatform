dc1d8cf576a8d2e7ccc5191cd13f1cc272acb1a6
Robert Nelson <robertcnelson@gmail.com>
2016-04-06 09:34:45 -0500

    4.1.21-armv7-lpae-x3 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,9 +24,9 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.1
-KERNEL_TAG=${KERNEL_REL}.20
+KERNEL_TAG=${KERNEL_REL}.21
 BUILD=${build_prefix}3
-kernel_rt=".15-rt17"
+kernel_rt=".20-rt23"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
