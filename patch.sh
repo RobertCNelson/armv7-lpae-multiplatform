c3a52999e075e2582195ed2805c6a92d6cde276e
Robert Nelson <robertcnelson@gmail.com>
2020-04-06 13:06:01 -0500

    stm32: merge in dts from mainline
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -334,6 +334,55 @@ beagleboard_dtbs () {
 	dir 'soc/ti/beagleboard_dtbs'
 }
 
+stm32_dtbs () {
+	branch="v5.4.x"
+	https_repo="https://github.com/RobertCNelson/stm32-DeviceTrees"
+	work_dir="stm32-DeviceTrees"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		cd ../
+		if [ ! -d ./${work_dir} ] ; then
+			${git_bin} clone -b ${branch} ${https_repo} --depth=1
+			cd ./${work_dir}
+				git_hash=$(git rev-parse HEAD)
+			cd -
+		else
+			rm -rf ./${work_dir} || true
+			${git_bin} clone -b ${branch} ${https_repo} --depth=1
+			cd ./${work_dir}
+				git_hash=$(git rev-parse HEAD)
+			cd -
+		fi
+		cd ./KERNEL/
+
+		#stm32 renamed a bunch...
+		rm -rfv ./arch/arm/boot/dts/stm32mp*
+
+		cp -vr ../${work_dir}/src/arm/* arch/arm/boot/dts/
+		cp -vr ../${work_dir}/include/dt-bindings/* ./include/dt-bindings/
+
+		${git_bin} add -f arch/arm/boot/dts/
+		${git_bin} add -f include/dt-bindings/
+		${git_bin} commit -a -m "Add stm32 DTBS: $branch" -m "${https_repo}/tree/${branch}" -m "${https_repo}/commit/${git_hash}" -s
+		${git_bin} format-patch -1 -o ../patches/soc/stm32_dtbs/
+		echo "STM32DTBS: ${https_repo}/commit/${git_hash}" > ../patches/git/STM32DTBS
+
+		rm -rf ../${work_dir}/ || true
+
+		${git_bin} reset --hard HEAD^
+
+		start_cleanup
+
+		${git} "${DIR}/patches/soc/stm32_dtbs/0001-Add-stm32-DTBS-$branch.patch"
+
+		wdir="soc/stm32_dtbs"
+		number=1
+		cleanup
+	fi
+
+	dir 'soc/stm32_dtbs'
+}
+
 local_patch () {
 	echo "dir: dir"
 	${git} "${DIR}/patches/dir/0001-patch.patch"
@@ -345,6 +394,7 @@ can_isotp
 wireguard
 ti_pm_firmware
 beagleboard_dtbs
+stm32_dtbs
 #local_patch
 
 pre_backports () {
