ad5d973a8ee9ee1275fd76d5a3ed9a67a6284825
Robert Nelson <robertcnelson@gmail.com>
2020-05-27 14:33:51 -0500

    kernel v5.4.40 rebase with: wireguard/can_isotp/device-tree/etc BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/da48f7407c40d191e6955c0d0247c06f77c6267e CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/ced84cab120a1ddde2bd4561a18514707c8a6184 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653 WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -442,7 +442,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.6.13"
+	backport_tag="v5.6.15"
 
 	subsystem="exfat"
 	#regenerate="enable"
@@ -534,7 +534,6 @@ reverts () {
 
 drivers () {
 	dir 'drivers/ar1021_i2c'
-	dir 'drivers/pwm'
 	dir 'drivers/sound'
 	dir 'drivers/spi'
 	dir 'drivers/tps65217'
