a325d92c79ce2ce04eb7375d155aebf8e21122af
Robert Nelson <robertcnelson@gmail.com>
2021-05-28 16:46:57 -0500

    kernel v5.12.8 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/dbe98caa11c0a3694f7833216b06a792127ff024 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/ac7a20f71e9d7971b1a376cfce8d43672a7e5ce1 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=04f3e5411b1cdc59292775e7b8e05a5d35ea4a80
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -557,6 +557,24 @@ backports () {
 	else
 		patch_backports
 	fi
+
+	backport_tag="v5.13-rc3"
+
+	subsystem="pru_rproc"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		pre_backports
+
+		cp -v ~/linux-src/drivers/irqchip/irq-pruss-intc.c ./drivers/irqchip/
+		cp -rv ~/linux-src/drivers/remoteproc/* ./drivers/remoteproc/
+		cp -v ~/linux-src/include/linux/remoteproc.h ./include/linux/remoteproc.h
+		cp -rv ~/linux-src/include/linux/remoteproc/* ./include/linux/remoteproc/
+
+		post_backports
+		exit 2
+	else
+		patch_backports
+	fi
 }
 
 reverts () {
