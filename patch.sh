435821222ca00167f7318eaae1319df7b38fd5c8
Robert Nelson <robertcnelson@gmail.com>
2022-01-27 16:02:57 -0600

    kernel v5.16.3 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/41291d0ae20e0a803a7d9c6cccd1bb3525f6439b BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/220e0613f49b50855d0bd34a93e433def4531853 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=2ce78ed90f71955f7b223c17b5cda6c8a7708efe
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -651,7 +651,7 @@ fixes
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.15.15"
+		backport_tag="v5.16.3"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
