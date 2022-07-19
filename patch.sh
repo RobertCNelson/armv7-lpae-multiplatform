bba8fbf3ad9069009299f4eac1dd7c3b3de79f09
Robert Nelson <robertcnelson@gmail.com>
2022-07-19 14:26:21 -0500

    kernel v5.17.15 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/63e0b7d716451d5712155b724caca18fb0faa045 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/2f159d7f0940ef18054d946e149008a2a4207d21 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=68a8f2f89cc58989bf4c1ade4757b2d5b680127e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -573,7 +573,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.10.127"
+	backport_tag="v5.10.131"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -613,7 +613,6 @@ drivers () {
 	#exit 2
 	dir 'RPi'
 	dir 'drivers/ar1021_i2c'
-	dir 'drivers/spi'
 	dir 'drivers/tps65217'
 
 	dir 'drivers/ti/serial'
@@ -627,7 +626,6 @@ drivers () {
 
 soc () {
 	dir 'soc/imx/imx7'
-	dir 'bootup_hacks'
 }
 
 fixes () {
@@ -644,7 +642,7 @@ fixes
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.18.8"
+		backport_tag="v5.18.12"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
