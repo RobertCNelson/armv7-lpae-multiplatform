0c7fb67c5613867c86e58c2fcb66a6d433fcd731
Robert Nelson <robertcnelson@gmail.com>
2025-10-16 15:20:37 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git pull --no-edit https://github.com/RobertCNelson/bb-kernel.git v6.12.x
-git pull --no-edit https://gitlab.com/RobertCNelson/bb-kernel.git v6.12.x
+git pull --no-edit https://github.com/RobertCNelson/armv7-lpae-multiplatform.git v6.12.x
+git pull --no-edit https://gitlab.com/RobertCNelson/armv7-lpae-multiplatform.git v6.12.x
 
